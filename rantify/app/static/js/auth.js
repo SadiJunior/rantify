2445f446491aab27f74e11284932679a5996ab32
Sadi Junior <sadilemesjr@gmail.com>
2024-07-23 01:22:15 -0300

    feat: add loading dots in log in page

---
+++ rantify/app/static/js/auth.js
@@ -0,0 +1,34 @@
+/**
+ * This file contains JavaScript functions for the Rantify auth.
+ */
+
+
+// Runs after DOM is fully loaded.
+$(document).ready(
+    function() {
+        onLoginButtonClicked();
+    }
+);
+
+
+// Runs when Log in Button is clicked
+function onLoginButtonClicked() {
+    $("#login-button").click(function(eventObject) {
+        displayLoadingDots();
+    });}
+
+
+// Displays the loading dots in index page via jQuery.
+function displayLoadingDots() {
+    const loadingDotsHtml = `
+        <div class="loading-dots"></div>
+    `;
+
+    $("#loading").html(loadingDotsHtml);
+}
+
+
+// Removes the loading dots in index page via jQuery.
+function removeLoadingDots() {
+    $(".loading-dots").remove();
+}
