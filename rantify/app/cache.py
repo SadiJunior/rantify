a7a5d0199400fd55c48a6a5606bb649c9c74c547
Sadi Junior <sadilemesjr@gmail.com>
2024-07-23 21:02:39 -0300

    feat: add cache middleware configuration for app

---
+++ rantify/app/cache.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+def init_app(app: Flask):
+    """Initialize cache middleware."""
+
+    @app.after_request
+    def after_request(response):
+        """Ensure responses aren't cached"""
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+
+        return response
